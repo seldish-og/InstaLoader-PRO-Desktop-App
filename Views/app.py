c37542832b421d97c1ab21efbf8a66eb3ab9f91f
tse-egor <82537848+tse-egor@users.noreply.github.com>
2021-11-07 21:13:06 +0300

    more done

--- Views/app.py
+++ Views/app.py
@@ -1,6 +1,26 @@
 import sys
 
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from design.design_app import Ui_MainWindow
 
 
+class MyApp(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.label.setText("OK")
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = MyApp()
+    form.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
