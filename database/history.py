17cdec1d86513de25677b93aea7d59a2510b5ec9
tse-egor <82537848+tse-egor@users.noreply.github.com>
2021-11-11 04:24:34 +0300

    deleted some trash

--- database/history.py
+++ database/history.py
@@ -26,23 +26,3 @@ class DataBase:
         self.cursor.close()
         self.connection.close()  
 
-# db = DataBase()
-# id = 1
-# mode = "image"
-# name = "file.mp4"
-# path = str(r"C:\Users\jegor\Desktop")
-# formated_date = "21:42/2021-11-10"
-
-# db.add_to_db(
-#     '''INSERT INTO history_data(id, mode, name, path, date) 
-#     VALUES (?,?,?,?,?);''', (id, mode, name, path, formated_date))
-
-
-# if not db.get_data_db('''SELECT * FROM history_data'''):
-#     print('hi')
-# db.add_to_db("UPDATE history_data SET id = id + 1 WHERE id != 5")
-# z = db.get_data_db('''SELECT * FROM history_data WHERE id = 1''')[0]
-# print(z[0]) 
-# db.add_to_db('''INSERT INTO history_data VALUES (1, "video", "file.mp4", "c:wwewenmnmnewk", "14:13/19.08")''')
-# db.delete_from_db('''DELETE FROM history_data WHERE id = 3''')
-# db.close_connection()
